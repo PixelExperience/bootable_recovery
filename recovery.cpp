e1beb1bdf8308302fbc41e3df6e0dcfd7bf126d2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-26 10:42:43 -0300

    Ignore signature/downgrade questions
    Since our recovery can be used to flash others roms, allow that
    
    Change-Id: Ia7d73b35e748d1c9a6e13c42fb87298c326ffff6

--- recovery.cpp
+++ recovery.cpp
@@ -184,22 +184,26 @@ static bool yes_no(Device* device, const char* question1, const char* question2)
   return (chosen_item == 1);
 }
 
-bool ask_to_continue_unverified(Device* device) {
+bool ask_to_continue_unverified(Device* device __unused) {
+  return true;
+  /*
   if (get_build_type() == "user") {
     return false;
   } else {
     ui->SetProgressType(RecoveryUI::EMPTY);
     return yes_no(device, "Signature verification failed", "Install anyway?");
-  }
+  }*/
 }
 
-bool ask_to_continue_downgrade(Device* device) {
+bool ask_to_continue_downgrade(Device* device __unused) {
+  return true;
+  /*
   if (get_build_type() == "user") {
     return false;
   } else {
     ui->SetProgressType(RecoveryUI::EMPTY);
     return yes_no(device, "This package will downgrade your system", "Install anyway?");
-  }
+  }*/
 }
 
 static bool ask_to_wipe_data(Device* device) {
