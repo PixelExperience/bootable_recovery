ca21b15df9f49925c4cc383bba6abb529f81cf4e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-04 12:23:07 -0300

    Enable downgrade checks only on incremental packages
    Change-Id: Ice3c8c832987718dc373b1a1b9a2f6b1faf7ee83

--- install/install.cpp
+++ install/install.cpp
@@ -140,22 +140,27 @@ static void ReadSourceTargetBuild(const std::map<std::string, std::string>& meta
 // incremental packages.
 static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& metadata) {
   // Incremental updates should match the current build.
-  auto device_pre_build = android::base::GetProperty("ro.build.version.incremental", "");
-  auto pkg_pre_build = get_value(metadata, "pre-build-incremental");
-  if (!pkg_pre_build.empty() && pkg_pre_build != device_pre_build) {
-    LOG(ERROR) << "Package is for source build " << pkg_pre_build << " but expected "
-               << device_pre_build;
-    return false;
+  auto is_incremental = !get_value(metadata, "pre-build").empty();
+  if (is_incremental){
+      LOG(INFO) << "Incremental package detected, checking compatibility...";
+      int64_t build_timestamp =
+      android::base::GetIntProperty("ro.build.date.utc", std::numeric_limits<int64_t>::max());
+      int64_t pkg_post_timestamp = 0;
+      // We allow to full update to the same version we are running, in case there
+      // is a problem with the current copy of that version.
+      auto pkg_post_timestamp_string = get_value(metadata, "post-timestamp");
+      if (pkg_post_timestamp_string.empty() ||
+          !android::base::ParseInt(pkg_post_timestamp_string, &pkg_post_timestamp) ||
+          pkg_post_timestamp < build_timestamp) {
+        if (get_value(metadata, "ota-downgrade") != "yes") {
+          LOG(ERROR) << "Incremental package is older than the current build, expected a build "
+                        "newer than timestamp "
+                     << build_timestamp << " but package has timestamp " << pkg_post_timestamp
+                     << " and downgrade not allowed.";
+          return false;
+        }
+      }
   }
-
-  auto device_fingerprint = android::base::GetProperty("ro.build.fingerprint", "");
-  auto pkg_pre_build_fingerprint = get_value(metadata, "pre-build");
-  if (!pkg_pre_build_fingerprint.empty() && pkg_pre_build_fingerprint != device_fingerprint) {
-    LOG(ERROR) << "Package is for source build " << pkg_pre_build_fingerprint << " but expected "
-               << device_fingerprint;
-    return false;
-  }
-
   return true;
 }
 
