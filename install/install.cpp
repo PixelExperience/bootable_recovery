dc9d00c7f20a88785ff4757507fc31817037b88d
jhenrique09 <jhsv09@gmail.com>
2021-12-08 22:29:18 -0300

    Revert "bootable: Read all asserts in case there are more than one"
    This reverts commit a217effcecf403a9f12de539fd3e12f746579753.
    
    Change-Id: Ic457addfaf2defbc0aa29326af45244fb8e510ef

--- install/install.cpp
+++ install/install.cpp
@@ -202,19 +202,10 @@ bool CheckPackageMetadata(const std::map<std::string, std::string>& metadata, Ot
     return false;
   }
 
-  // We allow the package to carry multiple product names split by ",";
-  // e.g. pre-device=device1,device2,device3 ... We will fail the
-  // verification if the device's name doesn't match any of these carried names.
   auto device = android::base::GetProperty("ro.product.device", "");
   auto pkg_device = get_value(metadata, "pre-device");
-  bool product_name_match = false;
-  for (const auto& name : android::base::Split(pkg_device, ",")) {
-    if (device == android::base::Trim(name)) {
-      product_name_match = true;
-      break;
-    }
-  }
-  if (!product_name_match) {
+  // device name can be a | separated list, so need to check
+  if (pkg_device.empty() || !isInStringList(device, pkg_device, FINGERPRING_SEPARATOR)) {
     LOG(ERROR) << "Package is for product " << pkg_device << " but expected " << device;
     return false;
   }
