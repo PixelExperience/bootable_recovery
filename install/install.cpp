87cda68816743642068c186cde6ae55087c89132
TH779 <i@779.moe>
2022-02-16 15:45:15 +0800

    install: Keep all spaces in "ui_print"
    Co-authored-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Id3cf9bdccf032ed42c0046666ed0dbcf68339920

--- install/install.cpp
+++ install/install.cpp
@@ -571,7 +571,9 @@ static InstallResult TryUpdateBinary(Package* package, bool* wipe_cache,
         LOG(ERROR) << "invalid \"set_progress\" parameters: " << line;
       }
     } else if (command == "ui_print") {
-      ui->PrintOnScreenOnly("%s\n", args.c_str());
+      // Keep all spaces
+      args = line.substr(space + 1);
+      ui->PrintOnScreenOnly("%s", args.c_str());
       fflush(stdout);
     } else if (command == "wipe_cache") {
       *wipe_cache = true;
